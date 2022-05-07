0e6686fdd1b7cbd84ebbcc4961387200b85b49d8
YaroslavPlaksii91 <yarosyk@ukr.net>
2022-05-07 11:54:43 +0300

    add localStorage

---
+++ src/components/ContactListElem/ContactListElem.jsx
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+import s from './ContactListElem.module.css';
+
+const ContactListElem = ({ name, number, id, onDelete }) => {
+  return (
+    <div className={s.listItem}>
+      <p className={s.name}>{name}:</p>
+      <span className={s.number}>{number}</span>
+      <button type="button" className={s.button} onClick={() => onDelete(id)}>
+        Delete
+      </button>
+    </div>
+  );
+};
+
+ContactListElem.propTypes = {
+  name: PropTypes.string.isRequired,
+  number: PropTypes.string.isRequired,
+  id: PropTypes.string.isRequired,
+  onDelete: PropTypes.func.isRequired,
+};
+
+export default ContactListElem;
